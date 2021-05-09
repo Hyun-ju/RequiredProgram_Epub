6da6dfa87c4f4c47ecb35d42bb496d56de30456e
김현주 <61423147+Hyun-ju@users.noreply.github.com>
2021-05-10 01:23:54 +0900

    v1.0.9
    SetUp 파일 포함

--- RemoveNewLine/WPF_RemoveLine/NewLineVM.cs
+++ RemoveNewLine/WPF_RemoveLine/NewLineVM.cs
@@ -1,7 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
